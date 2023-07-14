07bcf7757e0d7c45feba0af6a6a95fbc6c5212d1
Kevin Dietmeyer <kevindietmeyer@Kevins-MacBook-Air-2.local>
2023-07-14 10:14:34 -0400

    first commit

---
+++ app/(tabs)/gallery.tsx
@@ -0,0 +1,18 @@
+import { Pressable } from "react-native";
+import { Link } from "expo-router";
+import { Text, View } from "@/components/Themed";
+
+export default function Gallery() {
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24 }}>Gallery</Text>
+        <View style={{marginVertical: 16 }}>
+          <Link href="/albums" asChild>
+            <Pressable>
+              <Text>Go to Albums</Text>
+            </Pressable>
+          </Link>
+        </View>
+    </View>
+  )
+}
